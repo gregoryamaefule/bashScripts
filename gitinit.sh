2f51a0a0f81a54f3b4e9d3bd0410693980f0cd50
gregoryamaefule <amaefulegregory@gmail.com>
2023-05-15 10:42:29 +0100

    first commit

---
+++ gitinit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 'enter username'
+read user
+echo 'enter personal access token @ https://github.com/settings/tokens'
+read PATclassic
+echo 'enter repo name'
+read reponame
+
+
+git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/$user/$reponame.git
+git remote set-url origin  https://$user:$PATclassic@github.com/$user/$reponame.git
+git push -u origin main
